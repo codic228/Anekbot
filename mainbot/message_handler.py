e3e7aca51d300e942ae6db18d7fce9100c0e2f19
sosisk1 <oleglitvin2003@gmail.com>
2024-02-16 22:10:21 +0300

    f,j,f

--- mainbot/message_handler.py
+++ mainbot/message_handler.py
@@ -3,7 +3,7 @@ from telebot import types
 def action(bot, message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     btn1 = types.KeyboardButton("Случайный анекдот")
-    btn2 = types.KeyboardButton("Категории")
+    btn2 = types.KeyboardButton("Категори")
     markup.row(btn1, btn2)
     btn3 = types.KeyboardButton("Топ анекдотов")
     btn4 = types.KeyboardButton("Добавить анекдот")
